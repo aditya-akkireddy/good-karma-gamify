e2ee103dc7b0149e7d803a79ea43d04921ee5d49
aditya-akkireddy <tarunanandrao@gmail.com>
2025-05-28 16:37:35 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6EH4rS2KB1tlKDA79K8tc25rAds8zaGo",
+  authDomain: "deeditup-a7512.firebaseapp.com",
+  projectId: "deeditup-a7512",
+  storageBucket: "deeditup-a7512.firebasestorage.app",
+  messagingSenderId: "655197318756",
+  appId: "1:655197318756:web:df6726fd12e5b22387f971",
+  measurementId: "G-HG50P3N2GV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
