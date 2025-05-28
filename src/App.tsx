b6435282f9e186730aaf4bb1e3e6b9a9dc90e405
aditya-akkireddy <tarunanandrao@gmail.com>
2025-05-28 20:05:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -5,12 +6,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TokenProvider } from "./contexts/TokenContext";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GoodDeeds from "./pages/GoodDeeds";
 import Rewards from "./pages/Rewards";
 import About from "./pages/About";
 import Events from "./pages/Events";
+import Login from "./pages/Login"; 
 
 const queryClient = new QueryClient();
 
@@ -27,6 +30,7 @@ const App = () => (
             <Route path="/rewards" element={<Rewards />} />
             <Route path="/about" element={<About />} />
             <Route path="/events" element={<Events />} />
+            <Route path="/login" element={<Login />} /> {/* ✅ NEW ROUTE */}
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
