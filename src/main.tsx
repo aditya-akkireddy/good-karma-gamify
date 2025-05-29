0f602e8bb09f85ee014ad9dfd9bd38776a0b9614
aditya-akkireddy <tarunanandrao@gmail.com>
2025-05-29 22:40:29 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import { BrowserRouter } from "react-router-dom";
-import { TokenProvider } from "./contexts/TokenContext"; // ✅ ensure this path is correct
-
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find root element");
-
-createRoot(rootElement).render(
-  <BrowserRouter>
-    <TokenProvider>
-      <App />
-    </TokenProvider>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
